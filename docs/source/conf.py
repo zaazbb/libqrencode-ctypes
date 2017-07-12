c0adb5c677f07e321bc3d4dfc795498dfd7fdb30
zaazbb <zaazbb@163.com>
2017-07-12 11:46:18 +0800

    unicode strings translate to strings to support python3 in docs/source/conf.py.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,8 +40,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'libqrencode ctypes bindings'
-copyright = u'2012, Matthew Baker'
+project = 'libqrencode ctypes bindings'
+copyright = '2012, Matthew Baker'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -178,8 +178,8 @@ htmlhelp_basename = 'libqrencodectypesbindingsdoc'
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'libqrencodectypesbindings.tex', u'libqrencode ctypes bindings Documentation',
-   u'Matthew Baker', 'manual'),
+  ('index', 'libqrencodectypesbindings.tex', 'libqrencode ctypes bindings Documentation',
+   'Matthew Baker', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -211,6 +211,6 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'libqrencodectypesbindings', u'libqrencode ctypes bindings Documentation',
-     [u'Matthew Baker'], 1)
+    ('index', 'libqrencodectypesbindings', 'libqrencode ctypes bindings Documentation',
+     ['Matthew Baker'], 1)
 ]
